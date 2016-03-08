30117c6c64c4fd9951521ac92e92f42ceb06a23f
Drei <drei3000@gmail.com>
2016-03-08 12:20:54 -0700

    main Screen is created. need to start working on other screens.

--- main.py
+++ main.py
@@ -1,18 +1,72 @@
-import tkinter
-import sys
+'''
+Created on Mar 8, 2016
 
-class simpleapp_tk(tkinter.Tk):
-    def __init__(self,parent):
-        tkinter.Tk.__init__(self,parent)
-        self.parent = parent
-        self.initialize()
+@author: drei
+'''
 
-    def initialize(self):
-        self.grid()
-        self.entry = tkinter.Entry(self)
-        self.entry.grid(column=0,row=0,sticky='EW')
+from tkinter import *
 
+class mainScreen:
+    def __init__(self, parent):
+        frame = Frame(parent)
+        frame.pack()
+        
+        self.vehRegButton = Button(frame,
+                                      text = "New Vehicle Registration", 
+                                      fg = "black",
+                                      command = lambda: self.vehReg())
+        self.vehRegButton.pack(fill=X)
+
+        self.autoRegButton = Button(frame,
+                                      text = "Auto Transaction", 
+                                      fg = "black",
+                                      command = lambda: self.autoTrans())
+        self.autoRegButton.pack(fill=X)
+
+        self.DLRegButton = Button(frame,
+                                      text = "Driver License Registration", 
+                                      fg = "black",
+                                      command = lambda: self.DLReg())        
+        self.DLRegButton.pack(fill=X)
+
+
+        self.violationRecButton = Button(frame,
+                                      text = "Violation Record", 
+                                      fg = "black",
+                                      command = lambda: self.violationRec())
+        
+        self.violationRecButton.pack(fill=X)
+
+
+        self.searchEngButton = Button(frame,
+                                      text = "Search Engine", 
+                                      fg = "black",
+                                      command = lambda: self.searchEng())        
+        self.searchEngButton.pack(fill=X)
+
+        self.quitButton = Button(frame,
+                             text = "Quit",
+                             fg = "red",
+                             command = lambda: frame.quit)
+        self.quitButton.pack(fill=X)
+        
+    def vehReg(self):
+        print("New Vehicle Registration Button pressed.")
+        
+    def autoTrans(self):
+        print("Auto Transaction Button pressed.")
+        
+    def DLReg(self):
+        print("Driver Lincense Registration Button pressed.")
+        
+    def violationRec(self):
+        print("Violation Record Button pressed.")
+        
+    def searchEng(self):
+        print("New Vehicle Registration Button pressed.")
+        
+    
 if __name__ == "__main__":
-    app = simpleapp_tk(None)
-    app.title('my application')
-    app.mainloop()
+    root = Tk()
+    screen = mainScreen(root)
+    root.mainloop()
