1fc9cb9258645faeea8da402730d5fce07d18f79
Drei <drei3000@gmail.com>
2016-03-14 09:12:41 -0600

    JK, There's still the matter of registering a new owner if they don't exist. Working on that. A few minor tweaking with error handling. Then only owner reg left.

--- NewVehReg.py
+++ NewVehReg.py
@@ -25,54 +25,62 @@ class NewVehReg(tk.Frame):
         
         label = tk.Label(self, text = 'Register a New Vehicle:')
         label.grid(row = 0, column = 0, columnspan = 2)
+        
+        ownerLabel = tk.Label(self, text = 'Owner SIN: ')
+        ownerLabel.grid(row = 1, column = 0)
+        ownerText = tk.Text(self, height = heightText, width = widthText)
+        ownerText.config(bg = self.valid)
+        ownerText.grid(row = 1, column = 1)
+        ownerConfig = '^[a-zA-Z0-9]{1,15}$'
                 
         serialLabel = tk.Label(self, text = 'Serial Number: ')
-        serialLabel.grid(row = 1, column = 0)
+        serialLabel.grid(row = 2, column = 0)
         serialText = tk.Text(self, height = heightText, width = widthText)
         serialText.config(bg = self.valid)
-        serialText.grid(row = 1, column = 1)
-        serialConfig = '^\d{15}$'
+        serialText.grid(row = 2, column = 1)
+        serialConfig = '^[a-zA-Z0-9]{1,15}$'
           
         makerLabel = tk.Label(self, text = 'Vehicle Maker: ')
-        makerLabel.grid(row = 2, column = 0)
+        makerLabel.grid(row = 3, column = 0)
         makerText = tk.Text(self, height = heightText, width = widthText)
         makerText.config(bg = self.valid)
-        makerText.grid(row = 2, column = 1)
+        makerText.grid(row = 3, column = 1)
         makerConfig = '^[a-zA-Z0-9]{1,20}$'
           
         modelLabel = tk.Label(self, text = 'Model Number: ')
-        modelLabel.grid(row = 3, column = 0)
+        modelLabel.grid(row = 4, column = 0)
         modelText = tk.Text(self, height = heightText, width = widthText)
         modelText.config(bg = self.valid)
-        modelText.grid(row = 3, column = 1)
+        modelText.grid(row = 4, column = 1)
         modelConfig = '^[a-zA-Z0-9]{1,20}$'
          
           
         yearLabel = tk.Label(self, text = 'Year: ')
-        yearLabel.grid(row = 4, column = 0)
+        yearLabel.grid(row = 5, column = 0)
         yearText = tk.Text(self, height = heightText, width = widthText)
         yearText.config(bg = self.valid)
-        yearText.grid(row = 4, column = 1)
+        yearText.grid(row = 5, column = 1)
         yearConfig = '^\d{4}$'
           
           
         colorLabel = tk.Label(self, text = 'Color: ')
-        colorLabel.grid(row = 5, column = 0)
+        colorLabel.grid(row = 6, column = 0)
         colorText = tk.Text(self, height = heightText, width = widthText)
         colorText.config(bg = self.valid)
-        colorText.grid(row = 5, column = 1)
+        colorText.grid(row = 6, column = 1)
         colorConfig = '^[a-zA-Z]{1,10}$'
           
           
         typeLabel = tk.Label(self, text = 'Type_id: ')
-        typeLabel.grid(row = 6, column = 0)
+        typeLabel.grid(row = 7, column = 0)
         typeText = tk.Text(self, height = heightText, width = widthText)
         typeText.config(bg = self.valid)
-        typeText.grid(row = 6, column = 1)
+        typeText.grid(row = 7, column = 1)
         typeConfig = '^[1-9]\d*$'      
             
         submitBtn = tk.Button(self, text = 'Submit',
                             command = lambda: combineFuncs(submit(),
+                                                           validate(ownerText, ownerConfig),
                                                            validate(serialText, serialConfig),
                                                            validate(makerText, makerConfig),
                                                            validate(modelText, modelConfig),
@@ -81,13 +89,14 @@ class NewVehReg(tk.Frame):
                                                            validate(typeText, typeConfig),
                                                            validateDBserial(serialText),
                                                            validateDBtype(typeText),
+                                                           validateDBowner(ownerText),
                                                            pushDataToDB()))
                                                                         
-        submitBtn.grid(row = 7, column = 1)
+        submitBtn.grid(row = 8, column = 1)
         
         backBtn = tk.Button(self, text = 'Back',
                             command = lambda: controller.show_frame("MainMenu"))
-        backBtn.grid(row = 7, column = 0)
+        backBtn.grid(row = 8, column = 0)
         
         def submit():
             self.isValid = True
@@ -107,6 +116,18 @@ class NewVehReg(tk.Frame):
                 textField.config(bg = self.invalid)
                 self.isValid = False
                 
+        def validateDBowner(textField):
+            if self.isValid:
+                getOwner = 'SELECT sin FROM people'
+                ownerData = DBTables.getData(self, self.connectionStr, getOwner)
+                compStr = str(textField.get('1.0','end'))
+                for each in ownerData:
+                    print(str(each))
+                    if str(each) == compStr:
+                        self.isDBValid = False
+                        textField.config(bg = self.invalid)
+                        print('owner in DB')
+                        
         def validateDBserial(textField):
             if self.isValid:
                 getSerial = 'SELECT serial_no FROM vehicle'
